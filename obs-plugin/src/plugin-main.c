8e00804e2bdbf552f229e762e41b64faa80ee131
Erik Moqvist <erik.moqvist@gmail.com>
2022-12-29 16:18:42 +0100

    Playing around.

--- obs-plugin/src/plugin-main.c
+++ obs-plugin/src/plugin-main.c
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+Dr. Mario
+Copyright (C) 2022 erik.moqvist@gmail.com
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,17 +20,92 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include "plugin-macros.generated.h"
 
-OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+OBS_DECLARE_MODULE();
+
+struct test_filter {
+    obs_source_t *source;
+    gs_effect_t *whatever;
+};
+
+static const char *filter_getname(void *unused)
+{
+    UNUSED_PARAMETER(unused);
+    return "Dr. Mario";
+}
+
+static void filter_destroy(void *data)
+{
+    struct test_filter *tf = data;
+
+    if (tf) {
+        obs_enter_graphics();
+
+        gs_effect_destroy(tf->whatever);
+        bfree(tf);
+
+        obs_leave_graphics();
+    }
+}
+
+static void *filter_create(obs_data_t *settings, obs_source_t *source)
+{
+    UNUSED_PARAMETER(settings);
+
+    struct test_filter *tf = bzalloc(sizeof(struct test_filter));
+    char *effect_file;
+
+    obs_enter_graphics();
+
+    effect_file = obs_module_file("test.effect");
+
+    tf->source = source;
+    tf->whatever = gs_effect_create_from_file(effect_file, NULL);
+    bfree(effect_file);
+    if (!tf->whatever) {
+        filter_destroy(tf);
+        tf = NULL;
+    }
+
+    obs_leave_graphics();
+
+    return tf;
+}
+
+static void filter_render(void *data, gs_effect_t *effect)
+{
+    UNUSED_PARAMETER(effect);
+
+    struct test_filter *tf = data;
+
+    if (!obs_source_process_filter_begin(tf->source,
+                                         GS_RGBA,
+                                         OBS_ALLOW_DIRECT_RENDERING)) {
+        return;
+    }
+
+    obs_source_process_filter_end(tf->source, tf->whatever, 0, 0);
+}
+
+static struct obs_source_info test_filter = {
+    .id = "dr_mario",
+    .type = OBS_SOURCE_TYPE_FILTER,
+    .output_flags = OBS_SOURCE_VIDEO,
+    .get_name = filter_getname,
+    .create = filter_create,
+    .destroy = filter_destroy,
+    .video_render = filter_render,
+};
 
 bool obs_module_load(void)
 {
-	blog(LOG_INFO, "Dr mario plugin loaded successfully (version %s)",
-	     PLUGIN_VERSION);
-	return true;
+    blog(LOG_INFO,
+         "Dr mario plugin loaded successfully (version %s)",
+         PLUGIN_VERSION);
+    obs_register_source(&test_filter);
+    return true;
 }
 
 void obs_module_unload()
 {
-	blog(LOG_INFO, "Dr mario plugin unloaded");
+    blog(LOG_INFO, "Dr mario plugin unloaded");
 }
