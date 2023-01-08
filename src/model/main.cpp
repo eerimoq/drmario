3c475c5df583098924389135bbd59332be79596f
Erik Moqvist <erik.moqvist@gmail.com>
2023-01-08 19:45:39 +0100

    Playing around.

--- src/model/main.cpp
+++ src/model/main.cpp
@@ -1,4 +1,25 @@
 #include <cppflow/cppflow.h>
+#include <chrono>
+
+using namespace std::chrono;
+
+static void process(cppflow::model& model,
+                    const std::string& kind,
+                    cppflow::tensor& image)
+{
+    auto start = high_resolution_clock::now();
+    auto output = model(image);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    std::cout
+        << kind
+        << " "
+        << cppflow::arg_max(output, 1)
+        << " in "
+        << duration.count()
+        << "ms."
+        << std::endl;
+}
 
 int main()
 {
@@ -8,23 +29,22 @@ int main()
     auto cat = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     cat = cppflow::cast(cat, TF_UINT8, TF_FLOAT);
     cat = cppflow::expand_dims(cat, 0);
-    
+
     image = "jean-wimmerlin-Cdl7BWwATPg-unsplash.jpg";
     auto lion = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     lion = cppflow::cast(lion, TF_UINT8, TF_FLOAT);
     lion = cppflow::expand_dims(lion, 0);
-    
+
     image = "Green_Fly.jpg";
     auto fly = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     fly = cppflow::cast(fly, TF_UINT8, TF_FLOAT);
     fly = cppflow::expand_dims(fly, 0);
 
-    auto output = model(cat);
-    std::cout << "Cat: " << cppflow::arg_max(output, 1) << std::endl;
-    output = model(lion);
-    std::cout << "Lion: " << cppflow::arg_max(output, 1) << std::endl;
-    output = model(fly);
-    std::cout << "Fly: " << cppflow::arg_max(output, 1) << std::endl;
-    
+    for (int i = 0; i < 10; i++) {
+        process(model, "Cat", cat);
+        process(model, "Lion", lion);
+        process(model, "Fly", fly);
+    }
+
     return 0;
 }
