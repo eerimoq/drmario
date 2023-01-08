3804cda49def3eb1fa8d0ecb12284e3b6dce3b06
Erik Moqvist <erik.moqvist@gmail.com>
2023-01-08 20:02:53 +0100

    Playing around.

--- src/model/main.cpp
+++ src/model/main.cpp
@@ -25,17 +25,17 @@ int main()
 {
     cppflow::model model("model");
 
-    auto image = "my_cat.jpg";
+    auto image = "images/my_cat.jpg";
     auto cat = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     cat = cppflow::cast(cat, TF_UINT8, TF_FLOAT);
     cat = cppflow::expand_dims(cat, 0);
 
-    image = "jean-wimmerlin-Cdl7BWwATPg-unsplash.jpg";
+    image = "images/jean-wimmerlin-Cdl7BWwATPg-unsplash.jpg";
     auto lion = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     lion = cppflow::cast(lion, TF_UINT8, TF_FLOAT);
     lion = cppflow::expand_dims(lion, 0);
 
-    image = "Green_Fly.jpg";
+    image = "images/Green_Fly.jpg";
     auto fly = cppflow::decode_jpeg(cppflow::read_file(std::string(image)));
     fly = cppflow::cast(fly, TF_UINT8, TF_FLOAT);
     fly = cppflow::expand_dims(fly, 0);
